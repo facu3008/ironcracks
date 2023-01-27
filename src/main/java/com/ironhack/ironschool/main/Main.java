c8e1fa0a1a5f60ae1750a23a26142cce1b526f62
Facundo Moya <1986facundo@gmail.com>
2023-01-27 09:11:55 -0300

    Primer estructura en Main

--- src/main/java/com/ironhack/ironschool/main/Main.java
+++ src/main/java/com/ironhack/ironschool/main/Main.java
@@ -1,7 +1,46 @@
 package com.ironhack.ironschool.main;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el nombre de la escuela: ");
+        String schoolName  = scanner.nextLine();
+
+        System.out.println("Cuantos profesores pertenecen a la escuela? ");
+
+        int teacherQuant  = scanner.nextInt();
+        for (int i = 0; i<teacherQuant; i++ ){
+            System.out.println("Introduce el nombre del profesor: ");
+            String professorName  = scanner.nextLine();
+            System.out.println("Introduce el salario del profesor: ");
+            BigDecimal professorSalary = scanner.nextBigDecimal();
+        }
+
+        System.out.println("Cuantos cursos quieres dar en la escuela? ");
+        int courseQuant  = scanner.nextInt();
+
+
+        for (int i = 0; i<courseQuant; i++ ){
+            System.out.println("Introduce el nombre del curso: ");
+            String courseName  = scanner.nextLine();
+            System.out.println("Introduce el precio del curso: ");
+            BigDecimal coursePrice = scanner.nextBigDecimal();
+        }
+
+        System.out.println("Cuantos alumnos asisten la escuela? ");
+        int studentQuant  = scanner.nextInt();
+        for (int i = 0; i<studentQuant; i++ ){
+            System.out.println("Introduce el nombre del estudiante: ");
+            String studentName  = scanner.nextLine();
+            System.out.println("Introduce la direccion del estudiante: ");
+            String studentAddress  = scanner.nextLine();
+            System.out.println("Introduce el email del estudiante: ");
+            String studentEmail  = scanner.nextLine();
+        }
+
     }
+
 }
